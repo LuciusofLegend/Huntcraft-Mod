3705ce3185094e9dec084e15f8a5e61ecc694ff5
LuciusofLegend <luciusiber@gmail.com>
2023-04-25 18:31:18 -0400

    Debug

--- src/main/java/net/lucius/huntcraft/mixin/PotionWhitelistMixin.java
+++ src/main/java/net/lucius/huntcraft/mixin/PotionWhitelistMixin.java
@@ -1,11 +1,12 @@
 package net.lucius.huntcraft.mixin;
 
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potions;
 import net.minecraft.recipe.BrewingRecipeRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-public class PotionBlacklist {
+public class PotionWhitelistMixin {
 
-    
 }
