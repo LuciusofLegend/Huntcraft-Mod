46c0f2e0563076bd7700c6e03052f772743588ba
LuciusofLegend <luciusiber@gmail.com>
2023-05-08 10:38:39 -0400

    General progress.  Forgot to atomic commit :(

---
+++ src/main/java/net/lucius/huntcraft/command/PrintServerPathCommand.java
@@ -0,0 +1,26 @@
+package net.lucius.huntcraft.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import java.io.File;
+
+public class PrintServerPathCommand {
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("printpath")
+                .executes(PrintServerPathCommand::run));
+    }
+
+    private static int run(CommandContext<ServerCommandSource> context) {
+        String path = new File("").getAbsolutePath();
+        final Text text = Text.literal(path);
+
+        context.getSource().sendFeedback(text, true);
+        return 1;
+    }
+}
