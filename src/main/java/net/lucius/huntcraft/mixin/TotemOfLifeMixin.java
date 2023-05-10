ca652e5bd5f8c5dcc47edb016295f11b4c9a36eb
LuciusofLegend <luciusiber@gmail.com>
2023-05-10 16:24:49 -0400

    Revert "Remove TotemofLifeMixin (debugging)"
    This reverts commit 79b4294bf052b4b99fb008d1fef02246bf3eae07.

---
+++ src/main/java/net/lucius/huntcraft/mixin/TotemOfLifeMixin.java
@@ -0,0 +1,49 @@
+package net.lucius.huntcraft.mixin;
+
+
+import net.lucius.huntcraft.item.HuntcraftItems;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BooleanSupplier;
+
+import static com.google.common.collect.Iterables.size;
+
+@Mixin(MinecraftServer.class)
+public class TotemOfLifeMixin {
+
+    MinecraftServer server = (MinecraftServer) (Object) this;
+    List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    public void tick(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
+        for(int i = 0; i < size(players); i++) {
+            ServerPlayerEntity player = players.get(i);
+            hasTotem(player);
+        }
+    }
+
+    private static void hasTotem(ServerPlayerEntity player) {
+        Inventory inv = player.getInventory();
+
+        ItemStack totemstack = null;
+        for(int i = 0; i < 50; i++) {
+            ItemStack stack = inv.getStack(i);
+            if (stack.getItem().equals(HuntcraftItems.TOTEM_OF_LIFE)) {
+                totemstack = stack;
+                break;
+            }
+        }
+        if(totemstack != null) {
+
+        }
+    }
+}
