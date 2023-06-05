0d1e4ec822916f50125a970a7e07fe03fa1479ab
LuciusofLegend <luciusiber@gmail.com>
2023-06-05 16:43:19 -0400

    Reduce End Crystal explosion power

---
+++ src/main/java/net/lucius/huntcraft/mixin/EndCrystalTweaksMixin.java
@@ -0,0 +1,19 @@
+package net.lucius.huntcraft.mixin;
+
+import net.minecraft.entity.decoration.EndCrystalEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(EndCrystalEntity.class)
+public class EndCrystalTweaksMixin{
+
+    @ModifyArg(
+            method = "damage",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"),
+            index = 6
+    )
+    private float modifyCrystalExplosionPower(float power) {
+        return power - 3f;
+    }
+}
